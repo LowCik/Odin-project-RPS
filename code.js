aa5ad6e67632711b795dd521df1132143734ed15
LowCik <loup.ciosek@gmail.com>
2023-03-20 13:17:30 +0100

    Fix the logic and finish the exercise ! You can play now.

--- code.js
+++ code.js
@@ -1,21 +1,36 @@
 
 //initialiser le score 0-0
 let scoreP1, scoreP2 = scoreP1 = 0;
-console.log(scoreP1);
-console.log(scoreP2);
+let gameOn = true;
+
 function resetScore() {
+    gameOn = true;
     scoreP1 = 0;
     scoreP2 = 0;
 }
-function updateScore(scoreP1, scoreP2) {
+function updateScore() {
     console.log("The score is now :\n P1 : " + scoreP1 + " - " + scoreP2 + " P2");
 }
-function checkWinner() {
+function checkWinner(round) {
     if (scoreP1 >= 3) {
-        console.warn("PLAYER 1 WON THE BO3");
+        console.log("PLAYER 1 WON THE BO3 WITH " + scoreP1 + " TO " + scoreP2);
+        gameOn = false;
     } else if (scoreP2 >= 3) {
-        console.warn("PLAYER 2 WON THE BO3");
-    } else {
+        console.log("PLAYER 2 WON THE BO3 WITH " + scoreP2 + " TO " + scoreP1);
+        gameOn = false;
+    } else if (round >= 5) {
+        if (scoreP1 > scoreP2) {
+            console.log("PLAYER 1 WON THE BO3 WITH " + scoreP1 + " TO " + scoreP2);
+            gameOn = false;
+        } else if (scoreP2 > scoreP1) {
+            console.log("PLAYER 2 WON THE BO3 WITH " + scoreP2 + " TO " + scoreP1);
+            gameOn = false;
+        } else {
+            console.log("IT'S A FULL DRAW. Try again.");
+            gameOn = false;
+        }
+    }
+    else {
         return null;
     }
 }
@@ -37,7 +52,16 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
+function getPlayerChoice() {
+    let move = prompt("rock, paper, scissors !!").toLowerCase();
+    while (move != "rock" && move != "paper" && move != "scissors") {
+        move = prompt(
+            "please only choose between rock, paper or scissors"
+        ).toLowerCase();
+        console.log(move);
+    }
+    return move;
+}
 /*  function moveSelector(player, move) {
   while (move != "rock" && move != "paper" && move != "scissors") {
     move = prompt(
@@ -50,20 +74,27 @@ function getComputerChoice() {
 C'était quand j'utilisais prompt. je dois changer de méthode
  
 */
+function playARound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
 
-//let P1Choice = moveSelector("player 1", prompt("Le joueur 1 Choisi"));
+    console.log("computerChoice = " + computerChoice + " playerChoice = " + playerChoice);
 
-//let P2Choice = moveSelector("player 2", prompt("Le joueur 2 Choisi"));
+    playerChoice = playerChoice.toLowerCase(); // put everything to lowercase
+    console.log(whoWon(playerChoice, computerChoice));
+}
 
-//p1Answer.textContent = P1Choice;
-//p2Answer.textContent = P2Choice;
-winnerIs.textContent = whoWon(P1Choice, P2Choice);
-
+function game() {
+    resetScore();
+    for (round = 0; round <= 5 && scoreP1 < 3 && scoreP2 < 3 && gameOn; round++) {
+        playARound();
+        checkWinner();
+    }
+}
 /*
         Comparer P1 a P2.*/
-winnerIs.ontr
-function whoWon() {
-    let P1Choice = document.getElementById("P1Choice");
+
+function whoWon(P1Choice, P2Choice) {
     if (P1Choice === P2Choice) {
         return "Draw";
     } else if (
@@ -72,19 +103,25 @@ function whoWon() {
         (P1Choice === "paper" && P2Choice === "rock")
     ) {
         scoreP1++;
-        checkWinner;
+        updateScore();
+        return "Player win : " + P1Choice + " beats " + P2Choice;
     } else {
         scoreP2++;
-        checkWinner;
+        updateScore();
+        return "Computer win : " + P2Choice + " beats " + P1Choice;
     }
 }
-      /*
+/*
 Si égalité, pas de changement de score, et redemander aux joueurs.
 Si P1 gagne ajouter un point a P1
 Sinon ajouter un point a P2 (dans cet ordre. Egalité, P1 win, ou ajout de point a P2
 Check si P1 ou P2 a 3 le nombre de point désiré pour la victoire.
 Afficher le gagnant.
 
-  Ajouter un bouton de reset (reset les point et reprend a P1 qui choisie)
+Ajouter un bouton de reset (reset les point et reprend a P1 qui choisie)
 
-  */
+*/
+
+// Launch the game
+resetScore();
+game();
